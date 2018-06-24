9e4a05cd31886eb0a5edbb4dd7b47f67cf88112a
Jakub Kania <kubak.15@gmail.com>
2018-06-24 12:12:38 +0200

    api

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import * as api from './containers/api'
+import { withStyles, CssBaseline } from '@material-ui/core'
+
+import Layout from './components/layout'
+import LoginPage from './components/loginpage'
+import * as urls from './urls'
+
+
+window.zeppelin = api;
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-          test
-        </p>
+        <BrowserRouter>
+          <CssBaseline>
+            <Switch>
+              <Route path={urls.LOGIN} exact component={LoginPage}/>
+              <Route path={urls.ROOT} component={Layout} />
+            </Switch>
+          </CssBaseline>
+        </BrowserRouter>
       </div>
     );
   }
---
+++ src/components/layout.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { withStyles, TextField, Button } from '@material-ui/core';
+
+export default class Layout extends Component {
+  render() {
+    return (
+      <div>
+        <div>Layout view</div>
+      </div>
+    )
+  }
+}
