24df0965b261f969f9c73738075f2ffd594363cc
Jakub Kania <kubak.15@gmail.com>
2018-06-24 13:01:05 +0200

    login

--- src/containers/loginpage.js
+++ src/containers/loginpage.js
@@ -1,11 +1,48 @@
 import React, { Component } from 'react'
+import { withStyles, TextField, Button, WithStyles } from '@material-ui/core';
+import { withRouter } from 'react-router';
 
-export default class LoginPage extends Component {
+const styles = {
+  form: {
+    display: 'flex',
+    flexDirection: 'column',
+    minWidth: 400,
+  },
+  self: {
+    display: 'flex',
+    justifyContent: 'center',
+  }
+};
+
+class LoginPage extends Component {
+
+
   render() {
+    const { classes } = this.props;
+    
     return (
-      <div>
-        <div>Login view</div>
+      <div className={classes.self}>
+        <form>
+          <div className={classes.form}>
+            <TextField
+              label="Username"
+              name="username"
+              
+              onChange={this.handleChangeUsername}
+            />
+            <TextField
+              label="Password"
+              name="password"
+              
+              onChange={this.handleChangePassword}
+            />
+            <Button variant="raised" onClick={this.handleSubmit}>Login</Button>
+          </div>
+        </form>
       </div>
-    )
+    );
   }
 }
+
+
+export default withStyles(styles)(LoginPage);
